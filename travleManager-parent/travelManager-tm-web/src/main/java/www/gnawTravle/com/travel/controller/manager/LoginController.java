8af9b37b6a58b6d8a21e1fe03523105f92979c83
15398069231 <51853925+15398069231@users.noreply.github.com>
2020-06-19 15:37:51 +0800

    修改登录方法controller

--- travleManager-parent/travelManager-tm-web/src/main/java/www/gnawTravle/com/travel/controller/manager/LoginController.java
+++ travleManager-parent/travelManager-tm-web/src/main/java/www/gnawTravle/com/travel/controller/manager/LoginController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import www.gnawTravle.com.travel.entity.admin.Admin;
@@ -51,7 +50,7 @@ public class LoginController {
             model.addAttribute("msg", "用户名或密码错误");
             return "login";
         }
-        return null;
+        return "login";
     }
 
     /**
