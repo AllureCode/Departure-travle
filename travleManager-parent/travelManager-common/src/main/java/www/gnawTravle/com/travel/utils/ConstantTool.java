7978f49d75d2ea138d48524719bb4192bdf52d53
15398069231 <51853925+15398069231@users.noreply.github.com>
2020-06-19 15:34:34 +0800

    修改工具类

--- travleManager-parent/travelManager-common/src/main/java/www/gnawTravle/com/travel/utils/ConstantTool.java
+++ travleManager-parent/travelManager-common/src/main/java/www/gnawTravle/com/travel/utils/ConstantTool.java
@@ -10,4 +10,9 @@ public class ConstantTool {
     public static final String LOGIN_USERNAME_NOT_EXIST = "当前用户不存在";
     public static final Object ACCOUNT_DISABLE = "当前账户被停用" ;
     public static final Object USER_EXIST = "用户已存在" ;
+    public static final Object USER_LOGIN_NAME_PWD_NOT_NULL = "用户名密码不得为空!";
+    public static final Object USER_LOGIN_NAME_PWD_IS_ERROR ="用户名不存在或密码错误!" ;
+    public static final Object USER_LOGIN_ACC_CANCEL = "用户账户已注销";
+    public static final Object USER_PWD_IS_NOT_UNLIKE = "密码与确认密码不一致!";
+    public static final Object USER_REGISTER_IS_SUCCESS ="注册成功，请登陆!" ;
 }
